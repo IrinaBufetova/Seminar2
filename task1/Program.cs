7b0b1869adb98a91a51f3c5b87790b1bcb5cbbb7
Irina <tritty1@yandex.ru>
2023-08-11 22:58:34 +0300

    first_program

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Prompt (string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+return result;
+}
+int number = Prompt("Введите трехзначное число>");
+if(number<100||number>=1000)
+{
+    Console.WriteLine("Вы ввели не трехзначное число, пожалуйста, повторите ввод");
+    return;
+}
+Console.WriteLine($"Введенное число `{number}`");
+int secondRank = number / 10%10;
+Console.WriteLine($"Вторая цифра `{secondRank}`");
